2dc427c6191a47e350336fb1917f0b50423306aa
DavigJ <david.j.jung@gmail.com>
2023-12-03 12:30:22 -0500

    motley grill

--- src/main/java/com/davigj/onion_onion/core/registry/OOBlocks.java
+++ src/main/java/com/davigj/onion_onion/core/registry/OOBlocks.java
@@ -2,10 +2,16 @@ package com.davigj.onion_onion.core.registry;
 
 import com.davigj.onion_onion.core.OnionOnion;
 import com.teamabnormals.blueprint.core.util.registry.BlockSubRegistryHelper;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+import vectorwing.farmersdelight.common.block.FeastBlock;
 
 @Mod.EventBusSubscriber(modid = OnionOnion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class OOBlocks {
     public static final BlockSubRegistryHelper HELPER = OnionOnion.REGISTRY_HELPER.getBlockSubHelper();
-    //	public static final RegistryObject<Block> TEMPLATE_BLOCK = HELPER.createBlock("template_block", () -> new Block(Block.Properties.copy(Blocks.STONE)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> MOTLEY_GRILL_BLOCK = HELPER.createBlockNoItem("motley_grill_block", () -> new FeastBlock(
+            Block.Properties.copy(Blocks.CAKE), OOItems.MOTLEY_GRILL, true));
+
 }
--- src/main/java/com/davigj/onion_onion/core/registry/OOItems.java
+++ src/main/java/com/davigj/onion_onion/core/registry/OOItems.java
@@ -2,11 +2,16 @@ package com.davigj.onion_onion.core.registry;
 
 import com.davigj.onion_onion.core.OnionOnion;
 import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
+import vectorwing.farmersdelight.common.item.ConsumableItem;
+import vectorwing.farmersdelight.common.registry.ModEffects;
 
 @Mod.EventBusSubscriber(modid = OnionOnion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class OOItems {
@@ -17,4 +22,12 @@ public class OOItems {
 
     public static final RegistryObject<Item> ONION_RINGS = HELPER.createItem("onion_rings", () ->
             new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.7F).build())));
+
+    public static final RegistryObject<Item> MOTLEY_GRILL_BLOCK = HELPER.createItem("motley_grill_block", () -> new BlockItem(
+            OOBlocks.MOTLEY_GRILL_BLOCK.get(), new Item.Properties().stacksTo(1).craftRemainder(Items.IRON_TRAPDOOR).tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> MOTLEY_GRILL = HELPER.createItem("motley_grill", () -> new ConsumableItem(
+            new Item.Properties().food((new FoodProperties.Builder()).nutrition(11).saturationMod(0.7F)
+                    .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), 6000), 1.0F).build())
+                    .craftRemainder(Items.BOWL).stacksTo(16).tab(CreativeModeTab.TAB_FOOD), true));
 }
