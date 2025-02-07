0274e0fcbda52e034d6d6220ce496df4b2115615
DavigJ <david.j.jung@gmail.com>
2025-02-07 17:16:11 +0400

    i may have just hit the major leagues

--- src/main/java/com/davigj/onion_onion/core/OnionOnion.java
+++ src/main/java/com/davigj/onion_onion/core/OnionOnion.java
@@ -39,7 +39,7 @@ public class OnionOnion {
 
     private void commonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            OOCompat.registerCompat();
+            OOCompat.registerCompostables();
             initializeObbyMap();
         });
     }
--- src/main/java/com/davigj/onion_onion/core/other/OOCompat.java
+++ src/main/java/com/davigj/onion_onion/core/other/OOCompat.java
@@ -4,11 +4,7 @@ import com.davigj.onion_onion.core.registry.OOItems;
 import com.teamabnormals.blueprint.core.util.DataUtil;
 
 public class OOCompat {
-    public static void registerCompat() {
-        registerCompostables();
-    }
-
-    private static void registerCompostables() {
-        DataUtil.registerCompostable(OOItems.ONION_RINGS.get(), 0.3F);
+    public static void registerCompostables() {
+        DataUtil.registerCompostable(OOItems.ONION_SLICE.get(), 0.3F);
     }
 }
