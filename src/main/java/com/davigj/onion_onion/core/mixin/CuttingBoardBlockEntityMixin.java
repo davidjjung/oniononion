4b15c6ebcbda2ca858e4bfd44d943a1b4f428466
DavigJ <david.j.jung@gmail.com>
2025-04-25 16:43:18 +0400

    fixes mixin crash

--- src/main/java/com/davigj/onion_onion/core/mixin/CuttingBoardBlockEntityMixin.java
+++ src/main/java/com/davigj/onion_onion/core/mixin/CuttingBoardBlockEntityMixin.java
@@ -3,37 +3,41 @@ package com.davigj.onion_onion.core.mixin;
 import com.davigj.onion_onion.core.OOConfig;
 import com.davigj.onion_onion.core.other.tags.OOItemTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.BlockSource;
-import net.minecraft.core.Direction;
-import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vectorwing.farmersdelight.common.block.entity.CuttingBoardBlockEntity;
-import vectorwing.farmersdelight.common.block.entity.dispenser.CuttingBoardDispenseBehavior;
+import vectorwing.farmersdelight.common.block.entity.SyncedBlockEntity;
 import vectorwing.farmersdelight.common.tag.ForgeTags;
 
 import static com.davigj.onion_onion.core.other.OnionCutUtil.affectBlocks;
 import static com.davigj.onion_onion.core.other.OnionCutUtil.affectLivingEntities;
 
-@Mixin(CuttingBoardDispenseBehavior.class)
-public class CuttingBoardDispenseBehaviorMixin extends OptionalDispenseItemBehavior {
-    @Inject(method = "tryDispenseStackOnCuttingBoard", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;copy()Lnet/minecraft/world/item/ItemStack;"), remap = false)
-    private void dispenserBehavior(BlockSource source, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+@Mixin(CuttingBoardBlockEntity.class)
+public class CuttingBoardBlockEntityMixin extends SyncedBlockEntity {
+    public CuttingBoardBlockEntityMixin(BlockEntityType<?> tileEntityTypeIn, BlockPos pos, BlockState state) {
+        super(tileEntityTypeIn, pos, state);
+    }
+
+    @Inject(method = "processStoredItemUsingTool", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V"), remap = false)
+    private void a(ItemStack stack, Player player, CallbackInfoReturnable<Boolean> cir) {
         if (!OOConfig.COMMON.onionFun.get()) return;
-        Level level = source.getLevel();
-        BlockPos pos = source.getPos().relative((Direction)source.getBlockState().getValue(DispenserBlock.FACING));
+        Level level = this.getLevel();
+        BlockPos pos = this.worldPosition;
+        assert level != null;
         CuttingBoardBlockEntity board = (CuttingBoardBlockEntity) level.getBlockEntity(pos);
-        if (board != null) {
+        if (board != null && player == null) {
             boolean activateTears = false;
             ItemStack victim = board.getStoredItem();
             if (ModList.get().isLoaded("overweight_farming") && OOConfig.COMMON.bigOnion.get()) {
--- src/main/java/com/davigj/onion_onion/core/other/OnionCutUtil.java
+++ src/main/java/com/davigj/onion_onion/core/other/OnionCutUtil.java
@@ -20,6 +20,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraftforge.common.util.FakePlayer;
 
 import java.util.List;
 
@@ -51,7 +52,7 @@ public class OnionCutUtil {
                 ItemEntity ghastTearEntity = new ItemEntity(level, livingEntity.getX(), livingEntity.getY(),
                         livingEntity.getZ(), new ItemStack(Items.GHAST_TEAR));
                 level.addFreshEntity(ghastTearEntity);
-                if (player instanceof ServerPlayer serverPlayer) {
+                if (!(player instanceof FakePlayer) && player instanceof ServerPlayer serverPlayer) {
                     if (!player.getCommandSenderWorld().isClientSide()) {
                         OOCriteriaTriggers.ONION_GHAST.trigger((serverPlayer));
                     }
