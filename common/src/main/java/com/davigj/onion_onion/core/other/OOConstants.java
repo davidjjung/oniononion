c73dafb843a51de484eda07c760eeba60bc43595
Cassian Godsted <cassian@godsted.com>
2025-05-12 18:39:00 -0400

    Reimplement Frame Changer support

--- common/src/main/java/com/davigj/onion_onion/core/other/OOConstants.java
+++ common/src/main/java/com/davigj/onion_onion/core/other/OOConstants.java
@@ -1,5 +1,6 @@
 package com.davigj.onion_onion.core.other;
 
+import com.davigj.frame_changer.core.registry.FCBlocks;
 import dev.architectury.platform.Platform;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -12,19 +13,19 @@ public class OOConstants {
     public static void initializeObbyMap() {
         OBBY_MAP.put(Blocks.OBSIDIAN, Blocks.CRYING_OBSIDIAN);
         if (Platform.isModLoaded("frame_changer")) {
-//            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICKS.get(), FCBlocks.CRYING_OBSIDIAN_BRICKS.get());
-//            OBBY_MAP.put(FCBlocks.OBSIDIAN_PILLAR.get(), FCBlocks.CRYING_OBSIDIAN_PILLAR.get());
-//            OBBY_MAP.put(FCBlocks.CHISELED_OBSIDIAN.get(), FCBlocks.CRYING_CHISELED_OBSIDIAN.get());
-//            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN.get());
-//
-//            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_SLAB.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_SLAB.get());
-//            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_SLAB.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_SLAB.get());
-//
-//            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_STAIRS.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_STAIRS.get());
-//            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_STAIRS.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_STAIRS.get());
-//
-//            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_WALL.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_WALL.get());
-//            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_WALL.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_WALL.get());
+            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICKS.get(), FCBlocks.CRYING_OBSIDIAN_BRICKS.get());
+            OBBY_MAP.put(FCBlocks.OBSIDIAN_PILLAR.get(), FCBlocks.CRYING_OBSIDIAN_PILLAR.get());
+            OBBY_MAP.put(FCBlocks.CHISELED_OBSIDIAN.get(), FCBlocks.CRYING_CHISELED_OBSIDIAN.get());
+            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN.get());
+
+            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_SLAB.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_SLAB.get());
+            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_SLAB.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_SLAB.get());
+
+            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_STAIRS.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_STAIRS.get());
+            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_STAIRS.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_STAIRS.get());
+
+            OBBY_MAP.put(FCBlocks.OBSIDIAN_BRICK_WALL.get(), FCBlocks.CRYING_OBSIDIAN_BRICK_WALL.get());
+            OBBY_MAP.put(FCBlocks.POLISHED_OBSIDIAN_WALL.get(), FCBlocks.CRYING_POLISHED_OBSIDIAN_WALL.get());
         }
     }
 }
